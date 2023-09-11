48032f299b3ea80e4cc86363066b8bd43adfe970
ikhwanizao <ikhwanm32@gmail.com>
2023-09-11 12:17:29 +0800

    Change code to calculate age

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,35 @@
-const dayInput = document.getElementById("day");
-const monthInput = document.getElementById("month");
-const yearInput = document.getElementById("year");
 const submitBtn = document.querySelector(".js-submit-btn");
 
-const dayOutput = document.querySelector(".js-output-day");
-const monthOutput = document.querySelector(".js-output-month");
 const yearOutput = document.querySelector(".js-output-year");
+const monthOutput = document.querySelector(".js-output-month");
+const dayOutput = document.querySelector(".js-output-day");
 
 
 submitBtn.addEventListener("click", () => {
-    const day = parseInt(dayInput.value);
-    const month = parseInt(monthInput.value);
-    const year = parseInt(yearInput.value);
+  var d1 = document.getElementById("day").value;
+  var m1 = document.getElementById("month").value;
+  var y1 = document.getElementById("year").value;
 
-    const today = new Date();
-    const currentDay = today.getDate();
-    const currentMonth = today.getMonth() + 1;
-    const currentYear = today.getFullYear();
+  var date = new Date();
+  var d2 = date.getDate();
+  var m2 = 1 + date.getMonth();
+  var y2 = date.getFullYear();
+  var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-    let ageDay = currentDay - day;
-    let ageMonth = currentMonth - month;
-    let ageYear = currentYear - year;
+  if (d1 > d2) {
+    d2 = d2 + month[m2 - 1];
+    m2 = m2 - 1;
+  }
+  if (m1 > m2) {
+    m2 = m2 + 12;
+    y2 = y2 - 1;
+  }
+  var d = d2 - d1;
+  var m = m2 - m1;
+  var y = y2 - y1;
 
-    if (currentMonth < month || (currentMonth == month && currentDay < day)) {
-        ageYear--;
-        ageMonth += 11;
+  yearOutput.innerHTML = y;
+  monthOutput.innerHTML = m;
+  dayOutput.innerHTML = d;
 
-    }
-
-    dayOutput.innerHTML = ageDay;
-    monthOutput.innerHTML = ageMonth;
-    yearOutput.innerHTML = ageYear;
 });
